d0459b494267a68e86dff2f186e591fe0d946705
M.R ars <m.rafli.a.risdi.s@gmail.com>
2026-07-13 19:22:31 +0800

    add logic

--- #3 Random Number Generator/script/script.js
+++ #3 Random Number Generator/script/script.js
@@ -1,4 +1,11 @@
-let min = document.getElementById("btn-min")
-let max = document.getElementById("btn-max")
-let num = document.getElementById("angka")
+let min = document.getElementById("box-min");
+let max = document.getElementById("box-max");
+let num = document.getElementById("angka");
+const generator = document.getElementById("btn-generate");
 
+const ranNum = Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+generator.addEventListener("click", () => {
+    num = ranNum;
+});
